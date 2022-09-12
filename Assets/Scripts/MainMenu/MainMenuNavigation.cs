b8f6e52943410644e66dcf5bc2db9c21af4aed79
Alex Zhuang <azhuang639@gmail.com>
2022-09-12 14:15:58 +0200

    Added main menu scene

---
+++ Assets/Scripts/MainMenu/MainMenuNavigation.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuNavigation : MonoBehaviour
+{
+    public Button startButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);
+    }
+
+    void StartGame()
+    {
+        Debug.Log("button clicked!!!");
+        SceneManager.LoadScene("Alex");
+    }
+}
