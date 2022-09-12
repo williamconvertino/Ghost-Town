8868ffc5fc407631cf17ed7a8709afa442d24b28
Grace Li <gli@800G2-1767.DisStu.local>
2022-09-12 14:31:31 +0200

    player movement updated with gravity and height

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //horizontal speed of character
+    public float speed = 5f;
+
+    //public float jumpSpeed = 8f;
+    private float direction = 0f;
+
+    //height of jump 
+    public float jumpHeight= 3;
+
+    //how fast you go up
+    public float gravityScale = 10;
+
+    //how fast you call 
+    public float fallingGravityScale = 45;
+
+    private Rigidbody2D player;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+
     void Start()
     {
-        
+        player = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        direction = Input.GetAxisRaw("Horizontal");
+
+        float jumpForce = Mathf.Sqrt(jumpHeight * -2 * (Physics2D.gravity.y * player.gravityScale));
+
+        //left right movement 
+        if (direction > 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+        }
+        else if (direction < 0f)
+        {
+            player.velocity = new Vector2(direction * speed, player.velocity.y);
+        }
+        else
+        {
+            player.velocity = new Vector2(0, player.velocity.y);
+        }
+
+        //jumping movement 
+        if (Input.GetButtonDown("Jump") && isTouchingGround)
+        {
+            player.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+
+        //controlling how quickly it goes up and goes down 
+        if (player.velocity.y >= 0)
+        {
+            player.gravityScale = gravityScale;
+        }
+        else if (player.velocity.y < 0)
+        {
+            player.gravityScale = fallingGravityScale; 
+        }
     }
 }
