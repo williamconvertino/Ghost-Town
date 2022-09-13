c9a19ee2441d2b7053a8d5b17e560b62ad066c4b
William Convertino <williamconvertino@gmail.com>
2022-09-13 13:53:25 +0200

    Updated level 2

---
+++ Assets/Scripts/Ghosts/NewGhost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace Ghosts
+{
+    public class NewGhost: MonoBehaviour
+    {
+
+        public Vector3 finalPos;
+        private Vector3 _startPos;
+        private Vector3 direction;
+        public float speed;
+        private Rigidbody2D _rb2d;
+        private void Start()
+        {
+            _startPos = transform.position;
+            _rb2d = GetComponent<Rigidbody2D>();
+            direction = (finalPos - _startPos).normalized;
+        }
+
+        private void Update()
+        {
+            if (Vector3.Distance(transform.position, _startPos) > Vector3.Distance(finalPos, _startPos))
+            {
+                transform.position += direction * speed;
+            }
+
+            if (Vector3.Distance(transform.position, finalPos) > Vector3.Distance(finalPos, _startPos))
+            {
+                transform.position -= direction * speed;
+            }
+        }
+    }
+}
