cfe133c6aaaa147573c0a192e117ca40aef8fd8b
Alex Zhuang <azhuang639@gmail.com>
2022-09-13 14:58:15 +0200

    added scream

--- Assets/Scripts/Restart/RestartNavigation.cs
+++ Assets/Scripts/Restart/RestartNavigation.cs
@@ -8,10 +8,16 @@ public class RestartNavigation : MonoBehaviour
 {
     public Button restartLevelButton;
     public Button returnToMenuButton;
+    public AudioClip audioClip;
+
+    private AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+        StartCoroutine(PlayScream());
         restartLevelButton.onClick.AddListener(Restart);
         returnToMenuButton.onClick.AddListener(ReturnToMenu);
     }
@@ -24,6 +30,13 @@ public class RestartNavigation : MonoBehaviour
     void ReturnToMenu()
     {
         GameState.LoadLevelSelect();
+    }
+
+    IEnumerator PlayScream()
+    {
+        audioSource.Play();
+        yield return (new WaitForSeconds(0.5f));
+        audioSource.Stop();
     }
 
 }
